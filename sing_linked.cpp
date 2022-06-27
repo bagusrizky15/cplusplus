7f9e424ff67d0ab0dbe3f6ce4674ff24b981f1db
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-06-27 21:39:14 +0700

    .

--- sing_linked.cpp
+++ sing_linked.cpp
@@ -5,19 +5,19 @@ using namespace std;
 struct Buku
 {
     string judul, pengarang;
-    int tahun;
+    int tahunTerbit;
     Buku *next;
 };
-int main()
-{
+
     Buku *head, *tail, *cur, *newNode, *del, *before;
 
     //single linked list
-    void createSingleLinkedList(string judul, string pengarang, int tahun){
+    void createSingleLinkedList(string judul, string pengarang, int tB)
+    {
         head = new Buku();
         head->judul = judul;
         head->pengarang = pengarang;
-        head->tahunTerbit = tahun;
+        head->tahunTerbit = tB;
         head->next = NULL;
         tail = head;
     }
@@ -34,5 +34,42 @@ int main()
     return jumlah;
     };
     
-    
-}
+    //tambah awal single linked list
+    void addFirst(string judul, string pengarang, int tB){
+        newNode = new Buku();
+        newNode->judul = judul;
+        newNode->pengarang = pengarang;
+        newNode->tahunTerbit = tB;
+        newNode->next=head;
+        head = newNode;
+    }
+
+    void printSingleLinkedList(){
+        cout << "Jumlah data "<< countSingleLinkedList() << endl;
+        cur = head;
+        while (cur != NULL)
+        {
+            cout << "Judul Buku " << cur->judul << endl;
+            cout << "Pengarang Buku " << cur->pengarang << endl;
+            cout << "Tahun " << cur->tahunTerbit << endl;
+
+            cur = cur->next;
+        }
+        
+    }
+
+
+
+    int main(){
+        createSingleLinkedList("Kata", "Gaga", 2020);
+        printSingleLinkedList();
+
+
+        createSingleLinkedList("Kata", "Bagus", 2020);
+        printSingleLinkedList();
+
+
+        createSingleLinkedList("Kata", "Yiha", 2020);
+        printSingleLinkedList();
+    }
+
